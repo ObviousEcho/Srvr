60796ca4a159b2e09b5e791b75a72f3c7d97fbd5
Emmalee <emilee.wimmer@gmail.com>
2022-10-25 15:10:31 -0600

    added menu item routes

---
+++ controllers/api/menuItemsRoutes.js
@@ -0,0 +1,49 @@
+const router = require('express').Router();
+const {MenuItems, OrderItems, Orders, User } = require('../../models')
+module.exports = router;
+
+router.get('/', async(req, res) => {
+  const  menuItems = await MenuItems.findAll();
+  res.status(200).json(users);
+});
+
+router.get('/:id', async (req, res) => {
+
+  try{
+    const menuData = await MenuItems.findByPk(req.params.id);
+    if(!menuData){
+      res.status(404).json({ message: 'no Menu Item with id:'+req.params.id})
+    }
+    res.status(200).json(menuData);
+
+  }catch (err){
+    res.status(500).json(err)
+  }
+});
+
+router.post('/', (req, res) => {
+
+  MenuItems.create({
+    item_name: req.body.item_name,
+    price: req.body.price,
+    image_path: req.body.image_path
+  })
+  .then((newMenuItem) =>{
+    res.json(newMenuItem);
+  })
+  .catch((err)=>{
+    res.json(err);
+  })
+});
+
+router.delete('/:id', (req, res) => {
+  MenuItems.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((deletedMenuItem) => {
+      res.json(deletedMenuItem);
+    })
+    .catch((err) => res.json(err));
+});
