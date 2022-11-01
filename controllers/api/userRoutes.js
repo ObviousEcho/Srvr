a59b0ab1dc65e425ce2bce7bbea12255415e765b
Manamana <allentrobson@gmail.com>
2022-10-31 18:40:03 -0600

    posting new orders

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -74,6 +74,7 @@ router.post("/login", async (req, res) => {
     req.session.save(() => {
       req.session.user_id = userData.id;
       req.session.logged_in = true;
+      console.log(req.sessionID);
 
       res.json({ user: userData, message: "You are now logged in!" });
     });
